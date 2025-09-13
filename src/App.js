12094a5bb7695e814848c624a459837527702529
Jaggery Powder <elvreofficals@gmail.com>
2025-09-13 12:49:23 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import BuyNow from "./components/BuyNow";
+import WhatsApp from "./components/WhatsApp";    // new current change
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/buynow" element={<BuyNow />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/buynow" element={<BuyNow />} />
+        </Routes>
+      </Router>
+
+      <WhatsApp />        // new current change
+    </>
   );
 }
 
